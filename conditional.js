85a693ebab8197fc08f848bb75bed496ad666cae
faithyyy <faithfortune6@gmail.com>
2022-06-23 16:54:43 +0100

    Initial commit

---
+++ conditional.js
@@ -0,0 +1,7 @@
+let  myName = 'My name: Oloninayin Faith';
+let    myHeight = 'My height: 1.80cm';
+let    myCountry = 'My country: Nigeria';
+
+alert( myName, 
+    myHeight, 
+     myCountry)
---
+++ main.js
@@ -0,0 +1,15 @@
+let myName = 'Oloninayin Faith';
+console.log(myName)
+
+
+let myHeight = '1.80cm';
+console.log(myHeight)
+
+let myCountry = 'Nigeria';
+console.log(myCountry)
+
+alert(
+    `My name: ${myName}
+My height: ${myHeight}
+My country: ${myCountry}`
+    )
